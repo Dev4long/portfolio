fa0afb1ce4e07278b0809592ab97c5eb19ec3b10
denis kadrich <dev4long@live.com>
2022-05-28 11:58:10 -0400

    navbar almost

--- portfolio/src/Navbar.js
+++ portfolio/src/Navbar.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import './navbar.css'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className= "navbar">
+    <div className="container">
+        <h1>Denis Kadrich</h1>
+
+        <div className="menu">
+            <Link className="links" to="/">About</Link>
+            <Link className="links" to="/projects">Projects</Link>
+            <Link className="links" to="/resume">Resume</Link>
+            <Link className="links" to="/contact">Contact</Link>
+        </div>
+
+        <button class="burger">
+            <span></span>
+            <span></span>
+            <span></span>
+        </button>
+
+    </div>
+    </div>
   )
 }
 
