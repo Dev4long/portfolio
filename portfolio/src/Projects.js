f9859ad2102baa01268a76cab9fdc7c99a1d9cc1
denis kadrich <dev4long@live.com>
2022-06-16 17:14:11 -0400

    ok

--- portfolio/src/Projects.js
+++ portfolio/src/Projects.js
@@ -5,8 +5,23 @@ import './projects.css'
 function Projects() {
   return (
     <div className="projectContainer">
-      Projects
-   
+     
+      <div className="d1 row border">
+        
+        <img alt="Project" src="https://randomuser.me/api/portraits/men/5.jpg"/>
+        
+        {/* <div className="back">
+        <img src="https://randomuser.me/api/portraits/men/4.jpg"/>
+        </div> */}
+      </div>
+      
+      <div className="d2 row"></div>
+      <div className="d3 row"></div>
+      <div className="d4 row"></div>
+      {/* <div className="projectImage"></div>
+      <div className="projectImage"></div>
+      <div className="projectImage"></div>
+      <div className="projectImage"></div> */}
     </div>
   )
 }
