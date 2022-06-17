585052870a4386a96b68786305b2b3c98da8549a
denis kadrich <dev4long@live.com>
2022-06-17 17:38:57 -0400

    issues

--- portfolio/src/Projects.js
+++ portfolio/src/Projects.js
@@ -5,19 +5,28 @@ import './projects.css'
 function Projects() {
   return (
     <div className="projectContainer">
+  
      
-      <div className="d1 row border">
+      <div className="d1 row border first">
         
-        <img alt="Project" src="https://randomuser.me/api/portraits/men/5.jpg"/>
+        <div className="front">
+          <img className ="projectImage" alt="Project" src="https://randomuser.me/api/portraits/men/5.jpg"/>
+        </div>
         
-        {/* <div className="back">
-        <img src="https://randomuser.me/api/portraits/men/4.jpg"/>
-        </div> */}
+        <div className="back">
+        <img className ="projectImage" src="https://randomuser.me/api/portraits/men/4.jpg"/>
+        </div>
+
       </div>
-      
+    
+     
+     
       <div className="d2 row"></div>
+   
       <div className="d3 row"></div>
+  
       <div className="d4 row"></div>
+      
       {/* <div className="projectImage"></div>
       <div className="projectImage"></div>
       <div className="projectImage"></div>
