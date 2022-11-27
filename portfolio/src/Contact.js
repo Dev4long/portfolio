2418240b26c8858d36a07379fb8f90fe373d44cf
denis kadrich <dev4long@live.com>
2022-11-27 16:47:00 -0500

    almost done with contact

--- portfolio/src/Contact.js
+++ portfolio/src/Contact.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React from 'react';
+import './contact.css'
+import {AiFillGithub, AiFillLinkedin, AiOutlineMail, AiOutlinePhone} from 'react-icons/ai'
+
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div className="ContactContainer">
+      <div className="header">Contact info</div>
+   
+      <div className="linkInfo">
+        <AiFillGithub size={84}/>
+        <div>Github Profile</div>
+      </div>
+    
+      <div className="linkInfo">
+        <AiFillLinkedin size={84}/> 
+        <div>LinkiedIn Profile</div>
+      </div>
+    
+      <div className="linkInfo">
+        <AiOutlinePhone size={84}/>
+        <div>Cellphone number</div>
+      </div>
+    
+      <div className="linkInfo">
+        <AiOutlineMail size={84}/>
+        <div>Email</div>
+      </div>
+    
+    </div>
+
   )
 }
 
